82a667b99edf556d1e0ee7ef7982803c12f31470
LucasPln <lucas.paulin@hotmail.fr>
2019-10-07 19:37:01 +0200

    Initial commit

---
+++ Main.py
@@ -0,0 +1,150 @@
+from itertools import chain, combinations
+import time
+
+#### Fonctions ####
+
+def toutesLesRepartitions(n):
+    listePossibilite = []
+    nb2 = n//2
+    reste = n - (2 * nb2)
+
+    if(reste == 0):
+        queDesDeux = []
+        for i in range(nb2):
+            queDesDeux.append(2)
+
+        listePossibilite.append(queDesDeux)
+
+    nb2 = nb2 - 1
+
+    while (nb2 >= 0):
+        nb3 = 1
+        somme = 2*nb2 + 3*nb3
+
+        while (somme <= n):
+            if (somme == n):
+                possibilite = []
+
+                for i in range(nb3):
+                    possibilite.append(3)
+
+                for i in range(nb2):
+                    possibilite.append(2)
+
+                listePossibilite.append(possibilite)
+
+            nb3 += 1
+            somme = 2*nb2 + 3*nb3
+
+        nb2 = nb2 - 1
+
+    return listePossibilite
+    
+    
+
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+def afficherGroupesPossibles(listePossibilite, listeInt):
+    nbRep = 0
+
+    for repartition in listePossibilite:
+        solution = []
+        listeSolutions = []
+        uneSolution(repartition, listeInt, solution, listeSolutions)
+        nbRep += len(listeSolutions)
+        
+        for i in range(0,len(listeSolutions),3):
+        	if(len(listeSolutions) > i+2):
+        		print(listeSolutions[i], "   ", listeSolutions[i+1], "   ", listeSolutions[i+2])
+
+        	elif(len(listeSolutions) > i+1):
+        		print(listeSolutions[i], "   ", listeSolutions[i+1])
+
+        	else:
+        		print(listeSolutions[i])
+
+        
+
+    print(nbRep,"possibilites")
+
+def uneSolution(repartition, listeInt, solution, listeSolutions):
+
+	if(len(repartition) == 0):
+		if(estDoublon(solution,listeSolutions) == False):
+			listeSolutions.append(solution)    # Compte le nb de solutions trouvees
+
+	else :
+		tailleGroupe = repartition[0]
+		allSet = []
+
+		for s in list(powerset(listeInt)):
+			if(len(s) == tailleGroupe):
+				allSet.append(s)
+                
+		diviseur = repartition.count(tailleGroupe)
+
+		for k in range(len(allSet)//diviseur):
+			possibiliteSet = allSet[k]
+			solutionTemp = list(solution)
+			solutionTemp.append(possibiliteSet)
+			listeTemp = list(listeInt)
+                
+			for i in range(tailleGroupe):
+				listeTemp.remove(allSet[k][i])
+
+			newRepartition = list(repartition)
+			del newRepartition[0]
+
+			uneSolution(newRepartition, listeTemp, solutionTemp, listeSolutions)
+
+
+def estDoublon(solution, listeSolutions):
+	if(len(listeSolutions) == 0):
+		return False
+
+	for solutionTest in listeSolutions:
+
+		nb = 0
+
+		i = 0
+		while(i < len(solution)):
+			trouveCorres = False
+			for groupeSolTest in solutionTest:
+				if(solution[i] == groupeSolTest):
+					nb += 1
+					trouveCorres = True
+
+			if(trouveCorres == False):
+				break
+
+			i += 1
+
+		if(nb == len(solution)):
+			print(solution,"=",solutionTest)
+			return True
+
+	return False
+
+
+
+#### Programme principale ####
+start_time=time.time()
+
+n = 8
+
+repartitionPossibles = toutesLesRepartitions(n)
+
+#for repartition in repartitionPossibles:
+#    print(repartition)
+
+listeInt = []
+for i in range(1,n+1):
+    listeInt.append(i)
+
+#print(list(powerset(listeInt)))
+
+afficherGroupesPossibles(repartitionPossibles, listeInt)
+
+print("Temps d execution : %s secondes" % (time.time() - start_time))
